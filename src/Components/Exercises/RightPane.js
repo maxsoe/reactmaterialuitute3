5db66729fef20508c0ed31d22e1d2a2ce4c444cc
Max Soe <maxsoe@gmail.com>
2019-01-29 14:32:30 +0400

    02 - Grid Layout

---
+++ src/Components/Exercises/RightPane.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Paper } from "@material-ui/core";
+
+export default ({ style }) => <Paper style={style.Paper}>Right pane</Paper>;
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 const Header = props => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <AppBar position="static">
+    <Toolbar>
+      <Typography component="h1" color="inherit" variant="h5">
+        Exercise database
+      </Typography>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Header;
