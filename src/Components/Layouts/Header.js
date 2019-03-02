43abfc59357baf448dc078e762c9a77c897d5d02
Max Soe <maxsoe@gmail.com>
2019-03-02 19:44:00 +0400

    05 - Dialogs & Icons

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import CreateDialog from "../Exercises/Dialogs/Create";
 
 const Header = props => (
   <AppBar position="static">
     <Toolbar>
-      <Typography component="h1" color="inherit" variant="h5">
+      <Typography
+        component="h1"
+        color="inherit"
+        variant="h5"
+        style={{ flex: 1 }}
+      >
         Exercise database
       </Typography>
+      <CreateDialog />
     </Toolbar>
   </AppBar>
 );
